5a3f1341bc8a4ac060c422d63a158068e2c1d427
Bojack8789 <Francoferraro18@gmail.com>
2026-06-26 11:07:39 -0300

    Agregando tunel

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   
   // URL BASE
   const BASE_URL = import.meta.env.PROD 
-    ? 'https://wed-referenced-investigator-louis.trycloudflare.com'
+    ? 'https://wishing-division-cut-optics.trycloudflare.com'
     : 'http://localhost:5050';
   
   const API_URL = `${BASE_URL}/api`;
