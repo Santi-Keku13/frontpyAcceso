442a45f21d8411293efb0ec794501aa2d7406d3d
Bojack8789 <Francoferraro18@gmail.com>
2026-06-30 15:44:12 -0300

    Agregando tunel

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   
   // URL BASE
   const BASE_URL = import.meta.env.PROD 
-    ? 'https://coins-basket-presidential-complications.trycloudflare.com'
+    ? 'https://acute-bailey-wrapped-sec.trycloudflare.com'
     : 'http://localhost:5050';
   
   const API_URL = `${BASE_URL}/api`;
