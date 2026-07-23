fe682d0b93bdd9800e3ff2c5b0ce1af09bf5e50b
Bojack8789 <Francoferraro18@gmail.com>
2026-07-23 16:16:03 -0300

    Agregando tunel

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   
   // URL BASE
   const BASE_URL = import.meta.env.PROD 
-    ? 'https://webmasters-trace-vatican-fur.trycloudflare.com'
+    ? 'https://greetings-delegation-diffs-rendering.trycloudflare.com'
     : 'http://localhost:5050';
   
   const API_URL = `${BASE_URL}/api`;
