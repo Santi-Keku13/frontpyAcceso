d792019684d11d226922cdc2f6fae6cd28f06400
Bojack8789 <Francoferraro18@gmail.com>
2026-08-12 15:41:37 -0300

    Agregando tunel

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   
   // URL BASE
   const BASE_URL = import.meta.env.PROD 
-    ? 'https://breathing-things-recent-indication.trycloudflare.com'
+    ? 'https://essays-tomorrow-even-completion.trycloudflare.com'
     : 'http://localhost:5050';
   
   const API_URL = `${BASE_URL}/api`;
